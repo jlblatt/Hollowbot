378c3b62dea2dce6a558d71408be62a1d1d693b8
jlblatt <jlblatt+github@gmail.com>
2013-10-08 21:22:05 -0400

    Conf.py Removed from .gitignore

---
+++ conf.py
@@ -0,0 +1,19 @@
+import json
+
+try: confFile = open("conf.json", "r")
+except Exception, e:
+    print 'Error opening conf.json: %s'  % e
+    exit(1)
+
+confJSON = confFile.read()
+confFile.close()
+
+try: _ = json.loads(confJSON)
+except Exception, e:
+    print 'Error parsing conf.json: %s' % e
+    exit(1)
+
+# Check to make sure the user actually looked at the conf file
+if 'quit' in _:
+    print("Quit flag found in conf.json (you should probably at least look at the configuration before running, yeah?)")
+    exit(1)
